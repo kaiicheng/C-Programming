037820d514074ce063c0904368332a8e6b0de2cd
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-03-14 23:16:55 +0800

    Add files via upload

---
+++ Negative.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+	int i;
+	int j;
+	scanf("%d", &i);
+	j = -i;
+	printf("%d\n", j);
+}
---
+++ Two Digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+	int i;
+	int k;
+	scanf("%d", &i);
+	k = i / 10;
+	printf("%d\n", k);
+	k = i % 10;
+	printf("%d\n", k);
+}
