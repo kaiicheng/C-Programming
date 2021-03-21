5d0200c21b5fb732bc7f023d72a33d8bd1a0cda4
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-03-21 13:31:08 +0800

    Add files via upload

---
+++ Greatest Common Divisor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+	int i, j, k;
+	scanf("%d", &i);
+	scanf("%d", &j);
+	while (i % j != 0) {
+		k = i % j;
+		i = j;
+		j = k;
+	}
+	printf("%d\n", j);
+}
---
+++ if-then.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main(){
+	int i;
+	scanf("%d", &i);
+	if (i > 0)
+		printf("%d\n", i);
+}
