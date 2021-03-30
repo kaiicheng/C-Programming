2aee268915c7b4868d058e0e10b8b1e1958fea4e
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-03-30 23:16:25 +0800

    Add files via upload

---
+++ Tell Leap Year else-if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+	int year, k;
+	scanf("%d", &year);
+	if (year % 400 == 0)
+		k = 1;
+	else if ((year % 4 == 0) && (year % 100 != 0))
+		k = 1;
+	else
+		k = 0;
+	printf("%d\n", k);
+}
