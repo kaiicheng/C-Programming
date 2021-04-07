55787266208a6b3167bf6cb31a655528d8854034
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-04-07 23:18:07 +0800

    Add files via upload

---
+++ 2D Array Fill 0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main()
+{
+  int i, j;
+  int l, m;
+  int array1[3][3] = {{1, 2}, {4}};
+  int array2[3][3] = {{0}};
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      printf("%3d ", array1[i][j]);
+    printf("\n");
+  }
+  printf("\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      printf("%3d ", array2[i][j]);
+    printf("\n");
+  }
+  return 0;  /* this is a test */
+}
