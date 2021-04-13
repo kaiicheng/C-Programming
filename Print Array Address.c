e39d110977b5975f32fcced6eec012b58f5fbbc0
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-04-13 17:06:51 +0800

    Add files via upload

---
+++ Print Array Address.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+  int a[10];
+  int i;
+ 
+  printf("%d\n", sizeof(a[0]));
+  printf("%d\n", sizeof(a));
+  for (i = 0; i < 10; i++)
+    printf("%p\n", &(a[i]));
+  printf("%p\n", &a);
+  printf("%p\n", a);
+  return 0;
+}
+ 
---
+++ Print Array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main (){
+	int a[10];
+	int i;
+	for (i = 0; i < 10; i++)
+		scanf("%d", &(a[i]));
+	for (i = 0; i < 10; i++)
+	printf("%d\n", a[i]);	
+	
+}
