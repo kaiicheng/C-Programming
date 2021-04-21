b16f7ca681b32c91edf3f9c3315b5f4476dcefb5
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-04-22 00:18:09 +0800

    Add files via upload

---
+++ Multiple Inputs and Outputs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  int i;
+  float f;
+  double df;
+  scanf("%d%f%lf", &i, &f, &df);
+  printf("%d %p %f %f\n", i, &i, f, df);  /*%p and &i => address */
+  return 0;
+}
+ 
