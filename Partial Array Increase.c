09cd163f803d6526c5ddf9969109b4adf8b2151e
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-04-24 22:29:21 +0800

    Add files via upload

---
+++ Partial Array Increase.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void print_array(int a[], int n)
+{
+  int i;
+  for (i = 0; i < n; i++)
+    printf("a[%d] = %d\n", i, a[i]);
+  return;
+}
+void inc_array(int a[], int n)
+{
+  int i;
+  printf("inc_array: a = %p\n", a);
+  for (i = 0; i < n; i++)
+    a[i]++;
+  return;
+}
+int main(void)
+{
+  int i;
+  int array[5];
+  for (i = 0; i < 5; i++)
+    scanf("%d", &(array[i]));
+  printf("before inc_array\n");
+  print_array(array, 5);
+  inc_array(array, 5);
+  printf("after first inc_array\n");
+  print_array(array, 5);
+  inc_array(&(array[1]), 2);
+  printf("after second inc_array\n");
+  print_array(array, 5);
+  inc_array(&(array[2]), 2);
+  printf("after second inc_array\n");
+  print_array(array, 5);
+  return 0;
+}
+ 
---
+++ Print Elements of Array Function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void print_array(int array[], int n)
+{
+  int i;
+  printf("array is at %p\n", array);
+  for (i = 0; i < n; i++)
+    printf("array[%d] = %d\n", i, array[i]);
+  return;
+}
+int main(void)
+{
+  int i;
+  int a[3];
+  int b[5];
+  printf("main: a = %p\n", a);
+  printf("main: b = %p\n", b);
+  for (i = 0; i < 3; i++)
+    scanf("%d", &(a[i]));
+  for (i = 0; i < 5; i++)
+    scanf("%d", &(b[i]));
+  print_array(a, 3);
+  print_array(b, 5);
+  return 0;
+}
+ 
