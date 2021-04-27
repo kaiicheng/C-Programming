29b6b6b58e13245a9ea048d81cbc84506ad86bdd
Kai <33795285+kaiicheng@users.noreply.github.com>
2021-04-27 22:37:44 +0800

    Add files via upload

---
+++ Standard Library.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+main()
+{
+  int i, j;
+  double x, y;
+  scanf("%d", &i);
+  j = abs(i);
+  printf("%d\n", j);
+  scanf("%lf", &x);
+  y = sin(x);
+  printf("%f\n", y);
+  return 0;
+}
+ 
